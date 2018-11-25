dbc612b89dc7877d0037716eaffa5c15cdaad56e
stefanosoprana <stefano.soprana@icloud.com>
2018-11-25 20:26:18 +0100

    FizzBuzz - complete

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 // Scrivi un programma che
 // stampi i numeri da 1 a 100 ma per i multipli di
 // 3 stampi “Fizz” al posto del numero e per i
-// multipli di 5 stampi Buzz. Per i numeri che
+// multipli di 5 stampi "Buzz". Per i numeri che
 // sono sia multipli di 3 che di 5 stampi
-// FizzBuzz.
+// "FizzBuzz".
 
-// stampa i numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
 
-var multiplo3 = 3;
+  var quindici = (i % 15 == 0);
+  var tre = (i % 3 == 0);
+  var cinque = (i % 5 == 0);
 
-for (var i = 1; i < 100; i++) {
-  console.log(i);
-
-  if (i == multiplo3) {
-    multiplo3 = i + 3;
-    console.log('Fizz');
+  if (quindici) {
+    console.log('FizzBuzz');
+    document.writeln('FizzBuzz' + '<br>')
   }
-}
 
-console.log(i);
+  else if (tre) {
+    console.log('Fizz');
+    document.writeln('Fizz' + '<br>')
+  }
+
+  else if (cinque) {
+    console.log('Buzz');
+    document.writeln('Buzz' + '<br>')
+  }
+
+  else {
+    console.log(i);
+    document.writeln(i + '<br>')
+  }
+
+};
